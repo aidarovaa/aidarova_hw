8cca379e5524176ce3dc6dca198af091187b9423
aidarovaa <aidarovaaat@gmail.com>
2021-11-27 18:20:05 +0600

    dz7

--- dz7_aliia_08_01/oop.js
+++ dz7_aliia_08_01/oop.js
@@ -1,21 +1,56 @@
-// class Transport {
-//     constructor(color, model, size) {
-//         this.road = road;
-//         this.rail = rail;
-//         this.sea = sea;
-//         this.air = air;
-//     }
-// }
-//
-// const car = new Transport
-//
-//fff
-//
-//
-// road, ra
-// il, sea, air
-//
-
 class Transport {
-    startEngine()
+    constructor(name, color, model, size) {
+        this.name = name;
+        this.color = color;
+        this.model = model;
+        this.size = size;
+    }
+    startEngine(){
+        console.log("Engine is disable")
+    }
 }
+
+class Car extends Transport {
+    constructor(name, color, model, size) {
+        super(name, color, model, size);
+    }
+    startEngine() {
+        console.log("Engine is start")
+    }
+}
+
+const car = new Car("bmw", "white", "x6", 100)
+
+console.log(car)
+car.startEngine()
+
+
+
+class Yacht extends Transport {
+    constructor(name, color, model, size) {
+        super(name, color, model, size);
+    }
+    startEngine() {
+        console.log("Engine is start");
+    }
+}
+
+const yacht = new Yacht("Jack", "green", "12", 33)
+
+console.log(yacht)
+yacht.startEngine()
+
+class Motorbike extends Transport {
+    constructor(name, color, model, size) {
+        super(name, color, model, size);
+    }
+    startEngine() {
+        console.log("Engine is start")
+    }
+}
+
+const motorbike = new Motorbike("Becky", "aquamarine", "M100RR", 55)
+
+console.log(motorbike)
+motorbike.startEngine()
+
